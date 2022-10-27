a43fc890c9c0da0c7f1c96896a61d9d86a159f7e
Noah Pope <npope@tutanota.com>
2022-10-27 08:01:31 -0400

    Initial Commit

---
+++ WhatFlowerScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatFlowerScanner
+//
+//  Created by Noah Pope on 10/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            //added uiimagePickerControllerDelegate to VC
+    }
+
+
+}
+
