c45c00ad113caee1babbfd905040f03cbaef386e
Noah Pope <npope@tutanota.com>
2022-11-17 07:57:10 -0500

    Created folders for Flower Model and Flower Manager

---
+++ WhatFlowerScanner/Model/FlowerManager.swift
@@ -0,0 +1,10 @@
+//
+//  FlowerManager.swift
+//  WhatFlowerScanner
+//
+//  Created by Noah Pope on 11/17/22.
+//
+
+import Foundation
+
+let wikiURL = "https://en.wikipedia.org/w/api.php?"
---
+++ WhatFlowerScanner/Model/FlowerModel.swift
@@ -0,0 +1,9 @@
+//
+//  FlowerModel.swift
+//  WhatFlowerScanner
+//
+//  Created by Noah Pope on 11/17/22.
+//
+
+import Foundation
+
